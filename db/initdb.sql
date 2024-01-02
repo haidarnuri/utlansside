4984eecf02e73f815248786c78c4ece77a6ff20b
Haidar Nuri <haidarnuri@gmail.com>
2024-01-02 20:24:08 +0100

    changed docker-compose and added initdb.sql

---
+++ db/initdb.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS utlanITutstyr;
+USE utlanITutstyr;
+
+CREATE TABLE IF NOT EXISTS brukere(
+  brukerid INT AUTO_INCREMENT,
+  fornavn VARCHAR(255) NOT NULL,
+  etternavn VARCHAR(255) NOT NULL,
+  epost VARCHAR(255) NOT NULL,
+  telefonnummer VARCHAR(255),
+  klasse VARCHAR(150) NOT NULL,  
+  kontaktlarer VARCHAR(255) NOT NULL,
+  PRIMARY KEY(brukerid)
+);
+
+CREATE TABLE IF NOT EXISTS utstyr(
+  utstyrsid INT AUTO_INCREMENT,
+  typeutstyr VARCHAR(255) NOT NULL,
+  modell VARCHAR(255),
+  statusutstyr ENUM('tilgjengelig', 'utlånt', 'under vedlikehold') NOT NULL,
+  kjopsdato DATE,
+  sisteservice DATE,  
+  anneninfo varchar(255),
+  PRIMARY KEY(utstyrsid)
+);
+
+CREATE TABLE IF NOT EXISTS utlan(
+  utlansid INT AUTO_INCREMENT,
+  brukerid INT NOT NULL,
+  utstyrsid INT NOT NULL,
+  forventetreturdato DATE NOT NULL,
+  faktiskreturdato DATE,
+  CONSTRAINT fk_bruker_id FOREIGN KEY(brukerid)
+  REFERENCES brukere(brukerid)
+  ON UPDATE CASCADE,
+  CONSTRAINT fk_utstyr_id FOREIGN KEY(utstyrsid)
+  REFERENCES utstyr(utstyrsid)
+  ON UPDATE CASCADE,
+  PRIMARY KEY(utlansid)
+);
