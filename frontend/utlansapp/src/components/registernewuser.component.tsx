804c5a087f39492856385d27d5f1a08e34330783
Haidar Nuri <haidarnuri@gmail.com>
2024-01-30 09:59:38 +0100

    Started on connecting the backend API

--- frontend/utlansapp/src/components/registernewuser.component.tsx
+++ frontend/utlansapp/src/components/registernewuser.component.tsx
@@ -3,18 +3,34 @@ import React from "react";
 
 const RegisternewuserComponent = () => {
 
-const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-
-  };
+const [fornavn, setFornavn] = React.useState('');
+const [etternavn, setEtternavn] = React.useState('');
+const [mail, setMail] = React.useState('');
+const [tlf, setTlf] = React.useState('');
+const [kontaktlarer, setKontaktlarer] = React.useState('');
+const [password, setPassword] = React.useState('');
 
 return (
-    <form onSubmit={handleSubmit}>
+    <form >
       <label>
-        Username:
-        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+        Fornavn:
+        <input type="text" value={fornavn} onChange={(event) => setFornavn(event.target.value)} />
+      </label>
+      <label>
+        Etternavn:
+        <input type="text" value={etternavn} onChange={(event) => setEtternavn(event.target.value)} />
+      </label>
+      <label>
+        Skolemail:
+        <input type="text" value={mail} onChange={(event) => setMail(event.target.value)} />
+      </label>
+      <label>
+        tlf:
+        <input type="text" value={tlf} onChange={(event) => setTlf(event.target.value)} />
+      </label>
+      <label>
+        kontaktlærer:
+        <input type="text" value={kontaktlarer} onChange={(event) => setKontaktlarer(event.target.value)} />
       </label>
       <label>
         Password:
