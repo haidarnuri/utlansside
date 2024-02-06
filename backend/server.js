c855f605051f41878470f1917808ceb604f5676a
Haidar Nuri <haidarnuri@gmail.com>
2024-02-06 13:23:48 +0100

    created connection backend with db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const db = require('./db');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 4000;
+
 
 const corsOptions = {
   origin: 'http://localhost:3000', 
@@ -19,7 +21,21 @@ app.post('/register', (req, res) => {
   res.status(200).json({ message: 'User registered successfully!' });
 });
 
+
+// GET
+app.get('/brukere', async (req, res) => {
+  try {
+      const result = await db.pool.query("SELECT * FROM brukere");
+      res.send(result);
+  } catch (err) {
+    console.error("Error occurred while querying the database:", err.message);
+    res.status(500).json({message: "Error while querying the database", error: err.message});
+  }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
