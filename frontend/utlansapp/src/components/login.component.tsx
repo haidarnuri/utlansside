ddc1140fac98c51d7c645c8724656edc1d5857ca
Haidar Nuri <haidarnuri@gmail.com>
2024-03-05 16:37:02 +0100

    Able to get specific user from db from login component

--- frontend/utlansapp/src/components/login.component.tsx
+++ frontend/utlansapp/src/components/login.component.tsx
@@ -2,46 +2,48 @@ import React from "react";
 import { isValidEmail, isValidPassword } from "../utils/validation";
 import axios from "axios";
 
-
-
 const LoginComponent = () => {
+  const [mail, setMail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-const [mail, setMail] = React.useState('');
-const [password, setPassword] = React.useState('');
-
-
-
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if(!isValidEmail(mail)){
+    if (!isValidEmail(mail)) {
       console.log("Mailen er ikke riktig format!");
-    }
-    else if(!isValidPassword(password)){
-      console.log("Passord må ha minst et siffer, en stor og liten bokstav og ha minst 8 tegn");
-    }
-    else{
-      axios.get('http://localhost:4000/brukere')
-      .then(users => console.log(users.data))
-      .catch(err => console.log(err))
+    } else if (!isValidPassword(password)) {
+      console.log(
+        "Passord må ha minst et siffer, en stor og liten bokstav og ha minst 8 tegn"
+      );
+    } else {
+      console.log("mailen din er ", mail);
+      axios
+        .get(`http://localhost:4000/brukere/${mail}`)
+        .then((users) => console.log(users.data[0].epost))
+        .catch((err) => console.log(err));
     }
   };
 
-return (
+  return (
     <form onSubmit={handleSubmit}>
       <label>
         Mail:
-        <input type="text" value={mail} onChange={(event) => setMail(event.target.value)} />
+        <input
+          type="text"
+          value={mail}
+          onChange={(event) => setMail(event.target.value)}
+        />
       </label>
       <label>
         Password:
-        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </label>
       <button type="submit">Login</button>
     </form>
-    
   );
-
-}
-
+};
 
 export default LoginComponent;
